a78f8059e89e6992e0d406c4b5220ef0cf77b1de
EHAB3x <ehabmah6969@gmail.com>
2023-11-16 03:20:42 +0200

    create navbar and set content structure

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div className='finance__navbar'>
+        <h1>Finance</h1>
+    </div>
+  )
+}
+
+export default Navbar
